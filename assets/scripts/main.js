766794b9cf8ce254781322b2173cfceb77b50e37
mbrown-2 <66887136+mbrown-2@users.noreply.github.com>
2021-02-04 01:35:48 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,16 @@
 // main.js
 
 // TODO
+
+// Wtf am i supposed to do?
+
+
+// 1) input field for textual indicator of sound level
+
+// 2) Slider that serves visual controlller
+
+// 3) Volume icon that changes depending on sound level
+
+
+
+//
