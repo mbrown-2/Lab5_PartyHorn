2d6f5c5581154327f34ad3bc71d51ca9ca6577be
mbrown-2 <michaelbrown.5619@gmail.com>
2021-02-04 17:55:53 -0800

    first commit

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -13,4 +13,17 @@
 
 
 
-//
+// Implement connection between sound slider and input field.
+var output = document.getElementById("volume-number");
+var slider = document.getElementById("volume-slider");
+
+output.innerHTML = slider.value;
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
+
+// Implement connection between sound level and image
+
+// level 3 = 67-100
+// level 2 = 66-34
+// level 1 = 33-0
