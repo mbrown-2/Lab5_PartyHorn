ffddacd1080ba1fcd871c60526c6e6b310a9b8ba
mbrown-2 <66887136+mbrown-2@users.noreply.github.com>
2021-02-05 12:36:44 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -13,17 +13,22 @@ document.getElementById("volume-slider").onchange = function() {
 function noisePic(id) {
     var volPic = document.getElementById(id);
     var source = document.getElementById("volume-image");
+    var button = document.getElementById("honk-btn");
     if (volPic.value == 0) {
+        button.disabled = true;
         source.src = "./assets/media/icons/volume-level-0.svg";
     }
     else if (volPic.value >= 1 && volPic.value <= 33) {
         source.src = "./assets/media/icons/volume-level-1.svg";
+        button.disabled = false;
     }
     else if (volPic.value >= 34 && volPic.value <= 66) {
         source.src = "./assets/media/icons/volume-level-2.svg";
+        button.disabled = false;
     }
     else {
         source.src = "./assets/media/icons/volume-level-3.svg";
+        button.disabled = false;
     }
 }
 
@@ -41,16 +46,24 @@ document.getElementById("radio-party-horn-container").onclick = function() {
 
 function mainPic(id) {
     var picture = document.getElementById("sound-image");
+    var horn = document.getElementById("horn-sound");
     if (id == "radio-air-horn-container") {
         picture.src = "./assets/media/images/air-horn.svg";
+        horn.src = "./assets/media/audio/air-horn.mp3";
     }
     else if (id == "radio-car-horn-container") {
         picture.src = "./assets/media/images/car.svg";
+        horn.src = "./assets/media/audio/car-horn.mp3";
     }
     else {
         picture.src = "./assets/media/images/party-horn.svg";
+        horn.src = "./assets/media/audio/party-horn.mp3";
     }
 }
 
 // Play the sounds.
-
+document.getElementById("honk-btn").addEventListener("click", function(event) {
+    event.preventDefault();
+    var x = document.getElementById("horn-sound");
+    x.play();
+}
