eb7ba3be12023bc8c503524fefe18c06236bab53
mbrown-2 <66887136+mbrown-2@users.noreply.github.com>
2021-02-05 12:46:27 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -60,3 +60,10 @@ function mainPic(id) {
         horn.src = "./assets/media/audio/party-horn.mp3";
     }
 }
+
+// Play the sounds.
+document.getElementById("honk-btn").addEventListener("click", function(event) {
+    event.preventDefault();
+    var x = document.getElementById("horn-sound");
+    x.play();
+}
